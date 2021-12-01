bcc860b29209bac53d008fba2fc2184e9d0fd520
EmasikWothlan <yucong.wu@outlook.com>
2021-12-01 15:49:47 +0800

    removed djangoserver

--- MerveilleServer/porta_di_ferro/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortaDiFerroConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'porta_di_ferro'
--- MerveilleServer/porta_di_ferro/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from porta_di_ferro.views import porta_di_ferro, leave
-
-urlpatterns = [
-    path('', porta_di_ferro),
-    path('leave/', leave)
-]
